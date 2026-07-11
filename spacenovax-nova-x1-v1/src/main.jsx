119f5a9713a28e5077b8a761db34ef7a15331600
spacenovax <spacenovax@hotmail.com>
2026-07-12 05:42:48 +0900

    Add files via upload

---
+++ spacenovax-nova-x1-v1/src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import Game from './Game.jsx';
+import './style.css';
+createRoot(document.getElementById('root')).render(<Game/>);
